275634ad2bad7c02e4b10f19eb45c1dc852b9d0f
Sevangel <sevangelaad@gmail.com>
2022-06-24 12:29:12 +0200

    Theme creation

---
+++ wordpress/wp-content/themes/PettyCare/index.php
@@ -0,0 +1,9 @@
+<?php
+    get_header();
+?>
+<h1>Bienvenue sur le site Petty Care</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque totam ratione necessitatibus quae debitis libero voluptates ipsam aperiam quisquam repudiandae hic fugit, cumque reprehenderit quod vel, quaerat maiores! Officiis, ratione.</p>
+
+<?php
+    get_footer();
+?>
