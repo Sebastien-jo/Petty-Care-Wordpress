966bbd69d6045918f8eb849a4c5a5f4b37fb0b35
Sevangel <sevangelaad@gmail.com>
2022-06-30 11:41:58 +0200

    Wordpress/Theme/Assets and html

--- wordpress/wp-content/themes/PettyCare/header.php
+++ wordpress/wp-content/themes/PettyCare/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://use.typekit.net/ecx6sed.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Petty Care</title>
 </head>
