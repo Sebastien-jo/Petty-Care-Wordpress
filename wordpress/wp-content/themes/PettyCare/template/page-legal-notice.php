8b0dc72588eeb18072e83130b65ccf010b17bd01
Sevangel <sevangelaad@gmail.com>
2022-07-04 16:20:23 +0200

    Correction de bug lié au changement de place du fichier des mentions légales

--- wordpress/wp-content/themes/PettyCare/template/page-legal-notice.php
+++ wordpress/wp-content/themes/PettyCare/template/page-legal-notice.php
@@ -3,3 +3,77 @@
     Template Name: Legal Notice
 */
 ?>
+
+<?php
+get_header();
+?>
+
+<article>
+    <h1>Mentions légales</h1>
+    <div>
+        <p>Le site www.pettycare.com est la propriété de :</p>
+        <p>PETTY CARE capital social de 10000 €, 3 rue du Progrès 93100 Montreuil, RCS de Bobibny n°397 586 624</p>
+        <p>Numéro TVA intracommunautaire : FR 03 397586624</p>
+        <p>Téléphone : 0806 700 364 (coût d’un appel local depuis un poste fixe)</p>
+        <p>Directeur de la publication : XXXX XXXX</p>
+    </div>
+    <div>
+        <p>Ce site est hébergé par :</p>
+        <p>XXXXXX,</p>
+        <p>adresse,</p>
+        <p>téléphone</p>
+        <p>site internet</p>
+    </div>
+    <div>
+        <p>Coordonnées du service web :</p>
+        <p>Equipe web Petty Care</p>
+        <p>0806 700 364 (appel gratuit depuis un poste fixe, surcoût éventuel selon l'opérateur)</p>
+        <p>3 rue du Progrès 93100 Montreuil</p>
+    </div>
+    <p>
+        Le propriétaire de ce site se réserve le droit de modifier son contenu à tout moment et sans préavis. Il ne pourra être tenu responsable des conséquences de telles modifications. De même, il se réserve le droit d’interrompre ou de suspendre tout ou partie des fonctionnalités du site à tout moment et sans préavis.
+    </p>
+    <div>
+        <h2>INFORMATIONS FIGURANT SUR LE SITE</h2>
+        <p>
+            Le propriétaire du site fournit des informations à des fins purement informatives. Il s’efforce de contrôler leur exactitude et de les maintenir à jour. Mais aucune garantie n’est apportée concernant l’exactitude, la précision, la mise à jour ou l’exhaustivité de ces informations.
+        </p>
+        <p>
+            Par conséquent et à l’exception d’une faute lourde et intentionnelle, le propriétaire du site décline toute responsabilité pour tout dommage résultant notamment d’une imprécision ou inexactitude des informations disponibles sur ce site, ou pour toute atteinte résultant d’une intrusion frauduleuse d’un tiers sur ce site, ou encore pour tout dommage ou virus qui pourrait endommager ou rendre inutilisable votre équipement informatique suite à la visite de ce site.
+        </p>
+        <p>
+            Le propriétaire du site ne pourra être tenu responsable en cas d’indisponibilité du site pour quelque cause que ce soit.
+        </p>
+    </div>
+    <div>
+        <h2>LIENS VERS D’AUTRES SITES</h2>
+        <p>
+            Afin de faciliter l’accès à d’autres sites, le propriétaire du site a inséré un certain nombre de liens vers des sites tiers. Cependant, il n’est pas responsable de l’indisponibilité des sites tiers, de leur contenu, publicité ou autres éléments disponibles sur ces sites. La responsabilité du propriétaire de ce site ne saurait donc être engagée au titre d’un site tiers auquel vous auriez pu avoir accès.
+        </p>
+        <p>
+            Des sites externes peuvent contenir des liens hypertextes pointant vers ce site. Un tel lien ne pourra être installé sans un accord préalable et écrit du propriétaire de ce site.
+        </p>
+        <div>
+            <h2>PROPRIETE INTELLECTUELLE</h2>
+            <p>
+                L’accès à ce site vous confère un droit d’usage privé et non exclusif de ce site.
+            </p>
+            <p>
+                L’ensemble des éléments édités sur ce site, incluant notamment les textes, photographies, logos, marques, ... constituent des œuvres de l’esprit au sens du Code de la Propriété Intellectuelle. Par conséquent, toute utilisation sous forme de représentation, modification, reproduction, ... intégrale ou partielle, qui pourrait être faite sans le consentement de leurs auteurs ou de leurs ayants-droit, est illicite.
+            </p>
+            <p>
+                Ce site respecte les droits d’auteur. Tous les droits des auteurs des œuvres protégées reproduites et communiquées sur ce site, sont réservés.
+            </p>
+        </div>
+        <div>
+            <h2>DROIT APPLICABLE</h2>
+            <p>
+                Le site et son contenu sont créés conformément aux droit et règles applicables en France. Même s’il est accessible par d’autres utilisateurs, le site et son contenu sont destinés à l’accès et l’utilisation des résidents français.
+            </p>
+        </div>
+    </div>
+</article>
+
+<?php
+get_footer();
+?>
