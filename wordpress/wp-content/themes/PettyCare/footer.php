77a5fa25ff69f9497739c5c9ba52c6e246817006
Sevangel <sevangelaad@gmail.com>
2022-07-04 11:39:00 +0200

    Mise en place des templates sur toutes les pages html + homepage html

--- wordpress/wp-content/themes/PettyCare/footer.php
+++ wordpress/wp-content/themes/PettyCare/footer.php
@@ -1,2 +1,38 @@
 </body>
+<footer>
+    <div>
+        <!-- LOGO PETTY CARE -->
+    </div>
+    <p><a href="#">Nos produits</a></p>
+    <p><a href="#">Nos engagements</a></p>
+    <p><a href="#">Mon compte</a></p>
+    <p><a href="#">Devenir revendeur</a></p>
+    <p>Appelez-nous au 0806 700 364</p>
+    <p>Du lundi au vendredi de 10h à 18h</p>
+    <p>Ou contactez-nous via le formulaire en ligne</p>
+    <div>
+        <!-- BOUTON NOUS CONTACTER -->
+    </div>
+    <div>
+        <div>
+            <!-- COMPOSANT FACEBOOK -->
+        </div>
+        <div>
+            <!-- COMPOSANT INSTAGRAM -->
+        </div>
+        <div>
+            <!-- COMPOSANT TWITTER -->
+        </div>
+    </div>
+    <p><a href="#">@ Petty Care 2022</a></p>
+    <h5><a href="#">Plan du site</a></h5>
+    <h5><a href="#">Confidentialité</a></h5>
+    <h5><a href="#">Conditions générales de vente</a></h5>
+    <h5><a href="#">Mentions légales</a></h5>
+    <h5><a href="#">Gestion des cookies</a></h5>
+    <div>
+        <p>Paiement 100% sécurisé</p>
+        <!-- SVG LOGO MOYENS DE PAIEMENT -->
+    </div>
+</footer>
 </html>
--- wordpress/wp-content/themes/PettyCare/index.php
+++ wordpress/wp-content/themes/PettyCare/index.php
@@ -1,8 +1,7 @@
 <?php
     get_header();
 ?>
-<h1>Bienvenue sur le site Petty Care</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque totam ratione necessitatibus quae debitis libero voluptates ipsam aperiam quisquam repudiandae hic fugit, cumque reprehenderit quod vel, quaerat maiores! Officiis, ratione.</p>
+
 
 <?php
     get_footer();
