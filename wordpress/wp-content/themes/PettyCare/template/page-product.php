459fbe17ab8d6cbac1e0fda3a09b56dc98e527b6
Sevangel <sevangelaad@gmail.com>
2022-07-04 16:40:53 +0200

    Html et différentes version de la page

--- wordpress/wp-content/themes/PettyCare/template/page-product.php
+++ wordpress/wp-content/themes/PettyCare/template/page-product.php
@@ -3,3 +3,168 @@
     Template Name: Product
 */
 ?>
+
+<div>
+    <!-- Image background chat -->
+    <div>
+        <!-- Fleche de retour -->
+    </div>
+    <div>
+        <!-- Icone panier -->
+    </div>
+</div>
+<section>
+    <div>
+        <h4>Titre du produit</h4>
+        <h4>149 €</h4>
+    </div>
+    <div>
+        <!-- Etoiles des avis -->
+        <p>4.9/5</p>
+        <p><a href="#">Voir les avis</a></p>
+    </div>
+    <div>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+    </div>
+</section>
+<section>
+    <!-- Bouton quantité de produit -->
+    <!-- Bouton ajout au panier -->
+</section>
+
+<!-- SI CLIQUER SUR LE + -->
+<section>
+    <h4>Quantité</h4>
+    <span>1</span>
+    <span>2</span>
+    <span>3</span>
+</section>
+<section>
+    <!-- Bouton quantité de produit -->
+    <!-- Bouton ajout au panier -->
+</section>
+
+<!-- SI CLIQUER SUR AJOUT AU PANIER -->
+<section>
+    <div>
+        <h4>Titre du produit</h4>
+        <h4>149 €</h4>
+    </div>
+    <div>
+        <!-- Etoiles des avis -->
+        <p>4.9/5</p>
+        <p><a href="#">Voir les avis</a></p>
+    </div>
+    <div>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+    </div>
+</section>
+<section>
+    <!-- Bouton quantité de produit -->
+    <!-- svg de check -->
+    <p>Article ajouté au panier !</p>
+</section>
+
+<!-- APRES -->
+<section>
+    <div>
+        <h4>Titre du produit</h4>
+        <h4>149 €</h4>
+    </div>
+    <div>
+        <!-- Etoiles des avis -->
+        <p>4.9/5</p>
+        <p><a href="#">Voir les avis</a></p>
+    </div>
+    <div>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+    </div>
+</section>
+<section>
+    <!-- Bouton POURSUIVRE -->
+    <!-- Bouton ALLER AU PANIER -->
+</section>
+
+<!-- CLIQUER SUR POUSUIVRE -->
+<section>
+    <div>
+        <h4>Titre du produit</h4>
+        <h4>149 €</h4>
+    </div>
+    <div>
+        <!-- Etoiles des avis -->
+        <p>4.9/5</p>
+        <p><a href="#">Voir les avis</a></p>
+    </div>
+    <div>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+        <article>
+            <!-- image en cercle -->
+            <p>Lorem ipsum</p>
+        </article>
+    </div>
+</section>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor, elit sagittis, fermentum magna. Massa quisque in quisque vel laoreet sit nec, mi fermentum. Orci feugiat eget viverra tempor vel viverra cursus ultricies massa. Sem enim, scelerisque dictumst orci.</p>
+<h4>Découvrez aussi</h4>
+<!-- Image de chat -->
+<section>
+    <article>
+        <h4>Livraison et retours</h4>
+        <p>Livraison gratuite en France métropolitaine</p>
+        <p>Retours gratuits pendant 30 jours</p>
+    </article>
+    <article>
+        <h4>Service client à l'écoute</h4>
+        <p>Une question ?</p>
+        <p>Appelez-nous au 0806 700 364</p>
+        <p>Du lundi au vendredi, de 10h à 18h</p>
+    </article>
+    <article>
+        <h4>Paiement sécurisé</h4>
+        <p>Possible en 3x sans frais</p>
+    </article>
+</section>
+<section>
+    <!-- Bouton QUANTITE -->
+    <!-- Bouton ALLER AU PANIER -->
+</section>
