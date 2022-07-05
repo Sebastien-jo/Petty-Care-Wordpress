72f87ee9f0848b0595223505da225fffba7b6dab
Sevangel <sevangelaad@gmail.com>
2022-07-05 10:53:45 +0200

    commencement page recherche

--- wordpress/wp-content/themes/PettyCare/template/page-research.php
+++ wordpress/wp-content/themes/PettyCare/template/page-research.php
@@ -3,3 +3,14 @@
     Template Name: Research
 */
 ?>
+<?php
+    get_header();
+?>
+
+<section>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/Logo-Petty-Care-Viridian-Green.svg" alt="" height="100">
+</section>
+
+<?php
+    get_footer();
+?>
