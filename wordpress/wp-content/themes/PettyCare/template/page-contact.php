e0a500fb4f0deff6df87c92a13c134b4af3cd741
Sevangel <sevangelaad@gmail.com>
2022-07-05 14:48:49 +0200

    html formulaire

--- wordpress/wp-content/themes/PettyCare/template/page-contact.php
+++ wordpress/wp-content/themes/PettyCare/template/page-contact.php
@@ -6,9 +6,29 @@
 <?php
     get_header();
 ?>
-<div>
-    <?php echo do_shortcode("[ninja_form id=1]"); ?>
-</div>
+
+<form action="page-contact.php" method="post">
+    <div>
+        <label for="name">Prénom :</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="LastName">Nom :</label>
+        <input type="text" id="lastname" name="user_name">
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="email" id="mail" name="user_mail">
+    </div>
+    <div>
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="user_message"></textarea>
+    </div>
+    <div class="button">
+        <button type="submit">Envoyer le formulaire</button>
+    </div>
+</form>
+
 <?php
     get_footer();
 ?>
