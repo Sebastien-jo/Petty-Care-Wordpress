0faee568cc0030c25d869c92884a8887234ad592
Julie Cesana <juliecesana@MacBook-Air-de-Julie.local>
2022-07-05 11:29:03 +0200

    ajout html dashboard

--- wordpress/wp-content/themes/PettyCare/template/page-dashboard.php
+++ wordpress/wp-content/themes/PettyCare/template/page-dashboard.php
@@ -1,5 +1,26 @@
 <?php
-/*
-    Template Name: Dashboard
-*/
+get_header();
+?>
+
+<section>
+    <input type="button" value="Se déconnecter">
+</section>
+
+<section>
+    <div>
+        <a href="#">Historique et détails des commandes</a>
+    </div>
+    <div>
+        <a href="#">Mes adresses</a>
+    </div>
+    <div>
+        <a href="#">Mes informations personnelles</a>
+    </div>
+    <div>
+        <a href="#">Mes moyens de paiement</a>
+    </div>
+</section>
+
+<?php
+get_footer();
 ?>
