886529a84343f6cbed76fd3b925fa040f58b9b18
Sevangel <sevangelaad@gmail.com>
2022-07-05 11:01:00 +0200

    html page de recherche

--- wordpress/wp-content/themes/PettyCare/template/page-research.php
+++ wordpress/wp-content/themes/PettyCare/template/page-research.php
@@ -9,6 +9,20 @@
 
 <section>
     <img src="<?php echo get_template_directory_uri(); ?>/assets/Logo-Petty-Care-Viridian-Green.svg" alt="" height="100">
+    <!-- COMPOSANT CROIX -->
+    <p>X</p>
+</section>
+<section>
+    <input id="searchbar" onkeyup="search()" type="text" name="search" placeholder="Rechercher">
+    <p>Annuler</p>
+</section>
+<section>
+    <h3>Suggestions</h3>
+    <p>Lorem ipsum</p>
+    <p>Lorem ipsum</p>
+    <p>Lorem ipsum</p>
+    <p>Lorem ipsum</p>
+    <p>Lorem ipsum</p>
 </section>
 
 <?php
