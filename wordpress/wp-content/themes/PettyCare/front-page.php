2775e4f5e1e0d12e22cf8d884582109025b6b666
Sevangel <sevangelaad@gmail.com>
2022-07-06 10:32:08 +0200

    Ajout boostrap structure

--- wordpress/wp-content/themes/PettyCare/front-page.php
+++ wordpress/wp-content/themes/PettyCare/front-page.php
@@ -14,11 +14,9 @@
         <div>
             <h1>L'application santé pour votre animal de compagnie</h1>
         </div>
-        <div>
-            <!-- BOUTON COMMANDER -->
-            <div class="button">
-        <button type="button">Commander</button>
-    </div>
+        <!-- BOUTON COMMANDER -->
+        <div class="button">
+            <button type="button">Commander</button>
         </div>
     </div>
 </section>
@@ -43,87 +41,116 @@
     <div class="container">
         <div>
             <h2>Nos engagements</h2>
-            <article>
-                <!-- ARTICLE 1 -->
-                <p>Lorem ipsum dolor</p>
-            </article>
-            <article>
-                <!-- ARTICLE 2 -->
-                <p>Lorem ipsum dolor</p>
-            </article>
-            <article>
-                <!-- ARTICLE 3 -->
-                <p>Lorem ipsum dolor</p>
-            </article>
-            <article>
-                <!-- ARTICLE 4 -->
-                <p>Lorem ipsum dolor</p>
-            </article>
+            <div class="row">
+                <article class="col-md-3">
+                    <!-- ARTICLE 1 -->
+                    <p>Lorem ipsum dolor</p>
+                </article>
+                <article class="col-md-3">
+                    <!-- ARTICLE 2 -->
+                    <p>Lorem ipsum dolor</p>
+                </article>
+                <article class="col-md-3">
+                    <!-- ARTICLE 3 -->
+                    <p>Lorem ipsum dolor</p>
+                </article>
+                <article class="col-md-3">
+                    <!-- ARTICLE 4 -->
+                    <p>Lorem ipsum dolor</p>
+                </article>
+            </div>
         </div>
     </div>
 </section>
 <section class="operation">
     <div class="container">
-        <div>
-            <h2>Le fonctionnement</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tellus imperdiet ullamcorper cras bibendum donec. Suspendisse risus nullam nunc, viverra sapien nisl ultricies.</p>
-        </div>
-        <div>
-            <!-- UNE IMAGE -->
-        </div>
-        <div>
-            <!-- UN BOUTON -->
+        <h2>Le fonctionnement</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tellus imperdiet ullamcorper cras bibendum donec. Suspendisse risus nullam nunc, viverra sapien nisl ultricies.</p>
+                <!-- UN BOUTON -->
+                <div class="button">
+                    <button type="button">Découvrir</button>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <!-- UNE IMAGE -->
+            </div>
         </div>
     </div>
 </section>
 <section class="opinion">
     <div class="container">
-        <div>
-            <h2>Les avis clients</h2>
-            <!-- Condition, si on clique ça déroule les avis -->
-            <p><a href="">Voir tous les avis +</a></p>
+        <div class="row">
+            <div class="col-md-8">
+                <!-- UNE IMAGE -->
+            </div>
+            <div class="col-md-4">
+                <div>
+                    <h2>Les avis clients</h2>
+                    <!-- Condition, si on clique ça déroule les avis -->
+                    <p><a href="">Voir tous les avis +</a></p>
+                </div>
+                <article>
+                    <h4>Vanessa P.</h4>
+                    <div>
+                        <!-- LES ETOILES -->
+                    </div>
+                    <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tellus imperdiet ullamcorper cras bibendum donec. Suspendisse risus nullam nunc, viverra sapien nisl ultricies.”</p>
+                </article>
+                <article>
+                    <h4>Philippe E.</h4>
+                    <div>
+                        <!-- LES ETOILES -->
+                    </div>
+                    <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tellus imperdiet ullamcorper cras bibendum donec. Suspendisse risus nullam nunc, viverra sapien nisl ultricies.”</p>
+                </article>
+            </div>
         </div>
-        <article>
-            <h4>Vanessa P.</h4>
-            <div>
-                <!-- LES ETOILES -->
-            </div>
-            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tellus imperdiet ullamcorper cras bibendum donec. Suspendisse risus nullam nunc, viverra sapien nisl ultricies.”</p>
-        </article>
-        <article>
-            <h4>Philippe E.</h4>
-            <div>
-                <!-- LES ETOILES -->
-            </div>
-            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis tellus imperdiet ullamcorper cras bibendum donec. Suspendisse risus nullam nunc, viverra sapien nisl ultricies.”</p>
-        </article>
     </div>
 </section>
 <section class="newsletter">
     <div class="container">
-        <h2>Lorem newsletter inscription</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <input type="email" id='email' pattern='.+@gmail\.com' size='30' required>
-        <!-- BOUTON S'INSCRIRE -->
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Lorem newsletter inscription</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div class="col-md-6">
+                <input type="email" id='email' pattern='.+@gmail\.com' size='30' required>
+                <!-- BOUTON S'INSCRIRE -->
+                <div class="button">
+                    <button type="button">S'INSCRIRE</button>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 <section>
     <div class="container">
-        <article>
-            <h4>Livraison et retours</h4>
-            <p>Livraison gratuite en France métropolitaine</p>
-            <p>Retours gratuits pendant 30 jours</p>
-        </article>
-        <article>
-            <h4>Service client à l'écoute</h4>
-            <p>Une question ?</p>
-            <p>Appelez-nous au 0806 700 364</p>
-            <p>Du lundi au vendredi, de 10h à 18h</p>
-        </article>
-        <article>
-            <h4>Paiement sécurisé</h4>
-            <p>Possible en 3x sans frais</p>
-        </article>
+        <div class="row">
+            <div class="col-md-4">
+                <article>
+                    <h4>Livraison et retours</h4>
+                    <p>Livraison gratuite en France métropolitaine</p>
+                    <p>Retours gratuits pendant 30 jours</p>
+                </article>
+            </div>
+            <div class="col-md-4">
+                <article>
+                    <h4>Service client à l'écoute</h4>
+                    <p>Une question ?</p>
+                    <p>Appelez-nous au 0806 700 364</p>
+                    <p>Du lundi au vendredi, de 10h à 18h</p>
+                </article>
+            </div>
+            <div class="col-md-4">
+                <article>
+                    <h4>Paiement sécurisé</h4>
+                    <p>Possible en 3x sans frais</p>
+                </article>
+            </div>
+        </div>
     </div>
 </section>
 <?php
