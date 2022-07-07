884ec127877dcf5db759701252a4751e25016566
Sevangel <sevangelaad@gmail.com>
2022-07-07 10:29:39 +0200

    Page panier

--- wordpress/wp-content/themes/PettyCare/template/page-cart.php
+++ wordpress/wp-content/themes/PettyCare/template/page-cart.php
@@ -6,67 +6,117 @@
 <?php
     get_header();
 ?>
-
-<!-- LOGO PETTY CARE VIRIDIAN GREEN -->
-
-<div>
-    <!-- COMPOSANT FLECHE -->
-    <p><a href="#">Continuer mes achats</a></p>
-</div>
-<div>
-    <h2>Mon panier</h2>
-    <p>1 produit</p>
-</div>
-<section>
-    <article>
-        <!-- IMAGE CHAT -->
-    </article>
-    <article>
-        <h4>Nom du produit</h4>
-        <h4>149.00€</h4>
-        <div>
-            <label for="color-select">Couleur :</label>
-            <select name="pets" id="pet-select">
-                <option value="red">Rouge</option>
-                <option value="blue">Bleu</option>
-                <option value="green">Green</option>
-                <option value="yellow">Jaune</option>
-                <option value="pink">Rose</option>
-            </select>
+<div class="container">
+    <!-- LOGO PETTY CARE VIRIDIAN GREEN -->
+    
+    <div class="row">
+        <div class="col-md-2">
+            <!-- COMPOSANT FLECHE -->
         </div>
-        <div>
-            <p>Quantité</p>
-            <!-- BOUTON POUR AJOUTER QUANTITE -->
+        <div class="col-md-2">
+            <p><a href="#">Continuer mes achats</a></p>
         </div>
-    </article>
-</section>
-<section>
-    <h4>Récapitulatif</h4>
-    <div>
-        <input type="text" id='text' size='30' required>
-        <!-- BOUTON OK -->
     </div>
-    <div>
+    <div class = row>
+        <h2>Mon panier</h2>
         <p>1 produit</p>
-        <p>149.00€</p>
     </div>
-    <div>
-        <p>Date de livraison estimée avec Colissimo</p>
-        <p>13 juillet</p>
+    <section class="row">
+        <article class="col-md-6">
+            <!-- IMAGE CHAT -->
+        </article>
+        <article class="col-md-6">
+            <h4>Nom du produit</h4>
+            <h4>149.00€</h4>
+            <div>
+                <label for="color-select">Couleur :</label>
+                <select name="pets" id="pet-select">
+                    <option value="red">Rouge</option>
+                    <option value="blue">Bleu</option>
+                    <option value="green">Green</option>
+                    <option value="yellow">Jaune</option>
+                    <option value="pink">Rose</option>
+                </select>
+            </div>
+            <div class='row'>
+                <div class="col-md-2">
+                    <p>Quantité :</p>
+                </div>
+                <div class="col-md-1">
+                    <!-- BOUTON POUR AJOUTER QUANTITE -->
+                    <div class="button">
+                        <button type="button">-</button>
+                    </div>
+                </div>
+                <div class="col-md-1">
+                    <span>1</span>
+                </div>
+                <div class="col-md-1">
+                    <div class="button">
+                        <button type="button">+</button>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+    <section>
+        <h4>Récapitulatif</h4>
+        <div class="row">
+            <div class="col-md-3">
+                <input type="text" id='text' size='30' placeholder="J'ai un code avantage" required>
+            </div>
+            <div class="col-md-1">
+                <!-- BOUTON OK -->
+                <div class="button">
+                    <button type="button">Ok</button>
+                </div>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-md-10">
+                <p>1 produit</p>
+            </div>
+            <div class="col-md-2">
+                <p>149.00€</p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-md-10">
+                <p>Date de livraison estimée avec Colissimo</p>
+            </div>
+            <div class="col-md-2">
+                <p>13 juillet</p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-md-10">
+                <p>Frais de livraison estimés avec Colissimo</p>
+            </div>
+            <div class="col-md-2">
+                <p>Gratuit</p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-md-10">
+                <p>TOTAL</p>
+            </div>
+            <div class="col-md-2">
+                <p>149.00 €</p>
+            </div>
+        </div>
+    </section>
+    <!-- EN STATIQUE -->
+    <div class='row'>
+        <div class="col-md-3">
+            <h3>TOTAL : 149.00 €</h3>
+        </div>
+        <div class="col-md-3">
+            <!-- BOUTON COMMANDER -->
+            <div class="button">
+                <button type="button">Commander</button>
+            </div>
+        </div>
     </div>
-    <div>
-        <p>Frais de livraison estimés avec Colissimo</p>
-        <p>Gratuit</p>
-    </div>
-    <div>
-        <p>TOTAL</p>
-        <p>149.00 €</p>
-    </div>
-</section>
-<!-- EN STATIQUE -->
-<div>
-    <h3>TOTAL : 149.00 €</h3>
-    <!-- BOUTON COMMANDER -->
 </div>
 <?php
     get_footer();
